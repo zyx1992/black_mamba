c3da6b8f5b6a74de21b6bfd12bccb612717602d7
zhuyuxin <zhuyuxin.didichuxing.com>
2021-04-17 21:23:46 +0800

    任务列表

---
+++ src/api/ma/wallet.js
@@ -0,0 +1,14 @@
+import $http from '@/utils/ma/http.js'
+import { taskStatus } from '../../views/const'
+
+export function getTaskList(params) {
+  return $http.post('/task/list', params)
+}
+
+export function getTaskType() {
+  return taskStatus
+}
+
+export function deleteTask(id) {
+  return true
+}
